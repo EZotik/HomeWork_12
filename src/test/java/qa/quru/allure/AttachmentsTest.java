853c9b1bff34b4d8a8de647fa8741479a418c280
Ekaterina Zotova <karmengerl@gmail.com>
2024-02-16 23:28:47 +0300

    removing dependencies

--- src/test/java/qa/quru/allure/AttachmentsTest.java
+++ src/test/java/qa/quru/allure/AttachmentsTest.java
@@ -1,15 +1,12 @@
 package qa.quru.allure;
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.withText;
+import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
+
 
 
 public class AttachmentsTest {
